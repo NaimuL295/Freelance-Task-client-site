ac405e7a2cdc6585184778b9c87c8953a72bfc59
unknown <nayemk295@gmail.com>
2025-05-23 17:35:33 +0600

    add new

--- src/component/page/Login/Login.jsx
+++ src/component/page/Login/Login.jsx
@@ -1,8 +1,9 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import { AuthContext } from '../../context/AuthContext/AuthContext';
 import { Link, useNavigate } from "react-router";
   import { ToastContainer, toast } from 'react-toastify';
 const Login = () => {
+  const [err,setError ]=useState("")
  const {signinUser,signGoogle}=use(AuthContext)
   const navigate=useNavigate()
     const handlerLogin=(e)=>{
@@ -36,10 +37,14 @@ if (!email) {
 signinUser(email,password).then((result) => {
     console.log(result);
      navigate("/")
-}).catch((err) => {
-    console.log(err);
-    
-});
+}).catch((error) => { setError(error);
+
+       if (err.code ==="auth/email-already-in-use") {
+          toast.error("This email is already in use.");
+          if (err.code==="auth/invalid-email") {
+           toast.error("Invalid email address.");
+          }
+       }});
     }
 
 
@@ -68,13 +73,13 @@ const googleLogin=()=>{
           className="input w-full " placeholder="Password" required />
           <div><a className="link link-hover">Forgot password?</a>  <ToastContainer />     </div>
           <button type='submit'   className="btn bg-gray-800    btn-neutral mt-4">Log in      </button>
-
+  </form>
      <Link to="/signup">     <p className="text-xs text-center sm:px-6 dark:text-gray-600">Don't have an account?<span className="text-amber-600">Sign up now</span>  </p></Link>
       <button   onClick={googleLogin}    className="btn bg-white text-black border-[#e5e5e5]">
   <svg aria-label="Google logo" width="36" height="36" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
   Login with Google
 </button>
-        </form>
+      
       </div>
    </div>
         </div>
--- src/component/page/Signup/Signup.jsx
+++ src/component/page/Signup/Signup.jsx
@@ -8,6 +8,7 @@ import { CiRead } from "react-icons/ci";
   
 const Signup = () => {
 const {createUser,signGoogle,updateUser ,setUser}=use(AuthContext);
+const [err,setError ]=useState("")
 const [Toggle,setToggle]=useState(false)
   const navigate=useNavigate()
     const handlerSignup=(e)=>{
@@ -49,20 +50,32 @@ if (!name) {
 		// }
  createUser(email,password).
 	then(res=>{
-    
+   
 		const user=res.user
 		updateUser({  displayName:name,photoURL:photo}).then(() => {
 		   setUser({...user,displayName:name,photoURL:photo})
 		
-		}).catch((error) => {
-		  console.log(error);
-		 //  setUser(user)
-		})
+
+    
+
+
+		}).catch(() => {})
+     setTimeout(() => {
+       navigate("/") ;
+     toast.success("Register successful")
+     },1000);
 	  })
-	//   .catch((err) => {console.log(err);});
+ .catch((error) => { setError(error);
+
+       if (err.code ==="auth/email-already-in-use") {
+          toast.error("This email is already in use.");
+          if (err.code==="auth/invalid-email") {
+           toast.error("Invalid email address.");
+          }
+       }});
 	 
-	toast.success("Register successful")
-	 navigate("/")
+	
+
 	}
     
 const googleLogin=()=>{
@@ -102,12 +115,12 @@ const googleLogin=()=>{
              <Link to="/login">   <p className="text-xs text-center sm:px-6 dark:text-gray-600">Already have an account? <span className='text-amber-600'>log in</span></p>  </Link>
         </fieldset>
 
-        
-		<button    onClick={googleLogin} type='submit' className="btn bg-white text-black border-[#e5e5e5] space-y-2">
+      </form>   
+		<button    onClick={googleLogin}  className="btn bg-white text-black border-[#e5e5e5] space-y-2">
   <svg aria-label="Google logo" width="26" height="26" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
   Login with Google
 </button>
-    </form>
+   
 </div>
         </div>
     );
